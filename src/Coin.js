abfe6945869271a9dd35448132c1a8aef7508d24
Chengsi <gaochengsi@gmail.com>
2019-03-19 17:19:43 -0700

    initial commit

---
+++ src/Coin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Coin extends Component {
+    render () {
+        let imgageSrc = { imgUrl: this.props.imgURL };
+        return (
+            <div className="Coin">
+                <img src={imgageSrc} alt="coin-face"></img> 
+            </div>
+        )
+    }
+}
+
+export default Coin
---
+++ src/Coinflip.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Coin from ('./Coin');
+
+class CoinFlip extends Component {
+    
+}
+
+export default CoinFlip;
