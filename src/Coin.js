a1c24f97a7ce7a4e698766f8c5283bedcfb1ba04
Chengsi <gaochengsi@gmail.com>
2019-03-19 22:21:37 -0700

    added style

--- src/Coin.js
+++ src/Coin.js
@@ -4,8 +4,8 @@ import './Coin.css';
 class Coin extends Component {
     render () {
         return (
-            <div className="Coin">
-                <img className="Coin-img" src={this.props.imgUrl} alt="coin-face"></img> 
+            <div className="Coin text-center">
+                <img className="Coin-img img-fluid" src={this.props.imgUrl} alt="coin-face"></img> 
             </div>
         )
     }
--- src/Coinflip.js
+++ src/Coinflip.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Coin from './Coin';
+import './Coinflip.css';
 
 class CoinFlip extends Component {
     static defaultProps = {
@@ -12,7 +13,12 @@ class CoinFlip extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { coinImg: "http://cerc.net/_bopwp/wp-content/uploads/2013/12/158737094.jpg" };
+        this.state = { 
+            coinImg: "http://cerc.net/_bopwp/wp-content/uploads/2013/12/158737094.jpg",
+            totalFlip: 0,
+            totalHead: 0,
+            totalTail: 0
+        };
         this.handleClick = this.handleClick.bind(this);
     }
     
@@ -23,16 +29,25 @@ class CoinFlip extends Component {
 
     handleClick(evt) {
         const randomIdx = this.getRandomIdx();
+        if (randomIdx === 0) {
+            this.setState(st => ({ totalHead: st.totalHead +1 }));
+        }
+        else {
+            this.setState(st => ({ totalTail: st.totalTail + 1 }));
+        }
         const img = this.props.imgUrls[randomIdx];
-        this.setState(st => ({ coinImg: img }));
+        this.setState(st => ({ coinImg: img, totalFlip: st.totalFlip + 1}));
+        
     }
 
     render () {
         return (
-            <div className="CoinFlip">
-                <h3>Let's flip a coin!</h3>
-                <Coin imgUrl={this.state.coinImg}/>
-                <button className="CoinFlip-flip" onClick={this.handleClick}>Flip Meeee</button>
+            <div className="CoinFlip container">
+                <h3 className="text-center">Let's flip a coin!
+                    <Coin imgUrl={this.state.coinImg}/>
+                    <button type="button" className="CoinFlip-flip btn-small btn-primary" onClick={this.handleClick}>Flip Meeee</button>
+                    <h5 className="text-center">Out of {this.state.totalFlip} flips, there have been {this.state.totalHead} heads and {this.state.totalTail} tails.</h5>
+                </h3>
             </div>
         )
     }
